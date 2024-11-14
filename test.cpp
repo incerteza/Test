a8311e647ad8a7dfbaf8f41548408bd49ad19656
Tanya Volnina <tatyana.a.volnina@intel.com>
2024-11-14 16:45:46 +0100

    add headers

--- test.cpp
+++ test.cpp
@@ -1,17 +1,16 @@
-typedef struct _FILE {
-
-} FILE;
+#include <cstdlib>
+#include <cstdio>
 
 void test() {
-    FILE* f = 0;
-    int *p = (int*)malloc(10);
-    *p = 0;                        // Leak the pointer.
+   FILE* f = 0;
+   int *p = (int*)malloc(10);
+   *p = 0;                        // Leak the pointer.
 
-    f = fopen("file.txt", "w");    // Leak the file.
+   f = fopen("file.txt", "w");    // Leak the file.
 }
 
 int main(){
-        test();
-        return 0;
+   test();
+   return 0;
 }
 
