0402cebda235aa52c1159bd4cf6495dba0d074f0
TanyaVolnina <98888577+TanyaVolnina@users.noreply.github.com>
2024-11-14 15:57:10 +0000

    Only keep the leak

--- test.cpp
+++ test.cpp
@@ -2,11 +2,8 @@
 #include <cstdio>
 
 void test() {
-   FILE* f = 0;
    int *p = (int*)malloc(10);
    *p = 0;                        // Leak the pointer.
-
-   f = fopen("file.txt", "w");    // Leak the file.
 }
 
 int main(){
