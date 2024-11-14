3f148c28596d71af85b2a40b9eb02e0d0a7f09ee
Tanya Volnina <tatyana.a.volnina@intel.com>
2024-11-14 15:56:19 +0100

    null pointer dereference

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ all: a.out
 
 a.out:
 	$(CXX) test.cpp
+
+clean:
+	rm -f a.out
--- test.cpp
+++ test.cpp
@@ -1,4 +1,9 @@
+#include <cstddef>
+
 int main() {
     // Memory leak
-    int * q = new int;
+    int* q = new int;
+    // Null pointer dereference
+    int* v = NULL;
+    return *v;
 }
